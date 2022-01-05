b8003401159f03837068567b5450058b490ef658
balajivelu147 <balajimc22@gmail.com>
2022-01-05 18:33:40 +0530

    'styles

--- B17AEcom/src/app/app.component.ts
+++ B17AEcom/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'B17AEcom';
+  title2 = 'B17AEcom';
+
+  fun2() {
+    console.log('')
+  }
 }
