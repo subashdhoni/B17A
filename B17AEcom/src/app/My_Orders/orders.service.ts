b83c5c57eca1058002ac6fe842853f03a4219775
unknown <scboss97@gmail.com>
2022-01-14 08:09:04 +0530

    2nd commit from my orders page

--- B17AEcom/src/app/My_Orders/orders.service.ts
+++ B17AEcom/src/app/My_Orders/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,20 @@ export class OrdersService {
   constructor(private http:HttpClient) {  }
 
   getMyOrder(){
-    let url="/assets/data/order.json";
-    return this.http.get(url)
+    const headers= new HttpHeaders()
+  .set('content-type', 'application/json')
+
+  .set('Authorization', 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWJhc2giLCJ1c2VyaWQiOiI0MDMiLCJ1c2VyVHlwZUlkIjoiMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjIiLCJqdGkiOiJjNDRjMGUxNC05ZTFjLTRiMzYtODYwZi0xYjA2OTI5MDM4NDkiLCJleHAiOjE2NDIxMjc2ODksImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyJ9.4SoNzrAfGtUETrg4OycEhpPUYGaiF3ld_tH-fCiIJqE");
+    let url="https://bookcart.azurewebsites.net/api/Order/403";
+
+    // .set('Authorization', 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib3NzIiwidXNlcmlkIjoiNDA2IiwidXNlclR5cGVJZCI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIyIiwianRpIjoiZTJkZTRmODYtZDA5YS00N2U3LThmZjgtODU1NjMxZjMwNWNmIiwiZXhwIjoxNjQyMTI3OTQ1LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo0NDM2NC8ifQ.7fvzcDwnL7z_FIU6N2isdvcCZC6_vvdWOxcpoyqZjZo");
+    // let url="https://bookcart.azurewebsites.net/api/Order/406";
+
+    return this.http.get ( url , { 'headers': headers })
   }
 
   
 }
+//subash-403 //orders placed
+
+//boss-406  //no orders placed
--- B17AEcom/src/app/My_Orders/orders/orders.component.ts
+++ B17AEcom/src/app/My_Orders/orders/orders.component.ts
@@ -11,17 +11,19 @@ import { Data } from 'src/data';
 })
 export class OrdersComponent implements OnInit {
 
-
-
 displayedColumns: string[] = ['id', 'date', 'total'];
 
 dataSource =  new MatTableDataSource<Data>();
 
 loadData: boolean | undefined;
 
-
   @ViewChild(MatPaginator, { static: true })
   paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 
 constructor( private orderService:OrdersService) {  }
 
@@ -29,30 +31,28 @@ constructor( private orderService:OrdersService) {  }
    
 ngOnInit(): void {
   
-    this.loadData=false;
-    this.getMyOrders();
-    this.dataSource.paginator = this.paginator;
+    this.loadData=true;
+    this.getOrders();
     
   }
   
-getMyOrders(){
-  
+getOrders(){
     this.orderService. getMyOrder().subscribe((report:any)=>{
       if (report != null){
       this.dataSource.data=report as Data[]
-      this.loadData=true;
+      this.loadData=this.dataSource.data.length==0;
     }},
     error=>{
       console.log('Error Occured while fetching the My Order Details:',error);
     })
-   }
-
+  }
+  
   
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
+    const filterValue = (event.target as HTMLInputElement).value; 
     this.dataSource.filter = filterValue.trim().toLowerCase();
 }
-   
+
 }
 
 
