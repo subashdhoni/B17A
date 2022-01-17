c2e1fe500e855a1b8333f13b6fa8db42799ce1e1
unknown <scboss97@gmail.com>
2022-01-17 16:27:55 +0530

    removed unwanted imports

--- B17AEcom/src/app/My_Orders/orders.service.ts
+++ B17AEcom/src/app/My_Orders/orders.service.ts
@@ -13,10 +13,9 @@ export class OrdersService {
     const headers= new HttpHeaders()
   .set('content-type', 'application/json')
 
-  // .set('Authorization', 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWJhc2giLCJ1c2VyaWQiOiI0MDMiLCJ1c2VyVHlwZUlkIjoiMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjIiLCJqdGkiOiJjNDRjMGUxNC05ZTFjLTRiMzYtODYwZi0xYjA2OTI5MDM4NDkiLCJleHAiOjE2NDIxMjc2ODksImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyJ9.4SoNzrAfGtUETrg4OycEhpPUYGaiF3ld_tH-fCiIJqE");
-  //   let url="https://bookcart.azurewebsites.net/api/Order/403";
+ 
 
-    .set('Authorization', 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWJhc2giLCJ1c2VyaWQiOiI0MDMiLCJ1c2VyVHlwZUlkIjoiMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjIiLCJqdGkiOiI4OGViM2U3OS03ODZlLTQ0ZmItYTUzZC00ZWVhNmY4ZjI1M2IiLCJleHAiOjE2NDIxNjU2MTMsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyJ9.1WvFUJS1FS5k9RjkQ1-Am1MhrHUlXTaxd36EA03IrAc");
+    .set('Authorization', 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWJhc2giLCJ1c2VyaWQiOiI0MDMiLCJ1c2VyVHlwZUlkIjoiMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjIiLCJqdGkiOiI5NmYzZWZmYi1hM2Q2LTRlNDgtYjYxMC1kNmU2ZjRjMjQ1NDIiLCJleHAiOjE2NDI0MjA0OTcsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzY0LyJ9.fflvX9Nt_fpdnbtce4ATnDwIBjpEsnQzybY4E35ZOgo");
     let url="https://bookcart.azurewebsites.net/api/Order/403";
 
     return this.http.get ( url , { 'headers': headers })
--- B17AEcom/src/app/app.module.ts
+++ B17AEcom/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AgGridModule } from 'ag-grid-angular';
 import {MatNativeDateModule} from '@angular/material/core';
 import { OrdersComponent } from './My_Orders/orders/orders.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -24,7 +23,6 @@ import { MatInputModule } from '@angular/material/input';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatNativeDateModule,
-    AgGridModule.withComponents([]),
     HttpClientModule,
     MatTableModule,
     MatPaginatorModule,
